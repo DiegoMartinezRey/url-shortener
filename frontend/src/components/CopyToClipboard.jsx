fda6150f1846b0acf5ec35ef1b0f3ff40e7ee4e1
Diego Martinez <diegomrey1@gmail.com>
2024-04-15 22:05:49 +0200

    add icons from fontawsome

--- frontend/src/components/CopyToClipboard.jsx
+++ frontend/src/components/CopyToClipboard.jsx
@@ -1,3 +1,5 @@
+import { faCheck, faClipboard } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import React, { useEffect, useState } from "react";
 
 const CopyToClipboard = ({ urlInfo }) => {
@@ -22,14 +24,16 @@ const CopyToClipboard = ({ urlInfo }) => {
 
   return (
     <>
-      <div className="w-80 bg-gray-700 rounded-md py-2 px-4 flex justify-between">
+      <div className="w-80 bg-gray-700 rounded-md py-2 px-2 flex justify-between">
         <p className="text-white">{urlGetInfo.shortUrl && completeShortUrl}</p>
         <button
-          className="flex bg-white rounded-md cursor-pointer"
+          className="flex rounded-md cursor-pointer hover:bg-gray-500"
           onClick={() => copyToClipboard()}
           disabled={isCopied}
         >
-          <p className="text-black px-1">{!isCopied ? "Copy" : "Copied"}</p>
+          <p className="text-black px-1 text-white">
+            <FontAwesomeIcon icon={!isCopied ? faClipboard : faCheck} />
+          </p>
         </button>
       </div>
     </>
