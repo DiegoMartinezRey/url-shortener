ead8b91298b920523e80a1972805c3ddaa82942a
Diego Martinez <diegomrey1@gmail.com>
2024-04-30 01:05:28 +0200

    add cors header 2

--- backend/index.js
+++ backend/index.js
@@ -16,7 +16,7 @@ app.use(cors());
 app.use(function (req, res, next) {
   res.header(
     "Access-Control-Allow-Origin",
-    "https://url-shortener-sandy-eight.vercel.app/"
+    "https://url-shortener-7gof.vercel.app/"
   ); // Reemplaza con tu dominio de Vercel
   res.header(
     "Access-Control-Allow-Headers",
